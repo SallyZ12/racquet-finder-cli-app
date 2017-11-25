03b550ca8c96d0b7ab15cf7c7e13b224d33b99b9
Sally Zasloff <szasloff@gmail.com>
2017-11-25 15:04:03 +0000

    Done.

--- lib/racquet_finder/racquet.rb
+++ lib/racquet_finder/racquet.rb
@@ -2,4 +2,15 @@ class RacquetFinder::Racquet
 
   attr_accessor :brand, :model, :description, :price
 
+  @@racquets =[]
+
+  def initialize(brand = nil, model = nil, description = nil, price = nil)
+
+    @brand = brand
+    @model = model
+    @description = description
+    @price = price
+
+  end
+
 end
