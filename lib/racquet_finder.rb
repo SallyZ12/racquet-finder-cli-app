0ba7296c6f3476fd9c460399da7234012a87dfd7
Sally Zasloff <szasloff@gmail.com>
2017-11-26 15:36:25 +0000

    Done.

--- lib/racquet_finder.rb
+++ lib/racquet_finder.rb
@@ -1,6 +1,7 @@
 
 require 'open-uri'
 require 'nokogiri'
+require 'pry'
 
 
 require "racquet_finder/version"
--- lib/racquet_finder/cli.rb
+++ lib/racquet_finder/cli.rb
@@ -1,22 +1,28 @@
 class RacquetFinder::CLI
 
-
-  def call
-    # list_racquet_brands
-    list_racquet_models
+  #testing scraper to see if can scrape website
+  def scrape_brand
+    doc = Nokogiri::HTML(open("http://www.midwestsports.com"))
+    binding.pry
   end
-
-  def list_racquet_brands
-    puts "List of Tennis Racquet Brands"
-    puts "1. Babolat, 2. Head, 3. Wilson"
-  end
-
-  def list_racquet_models
-    # input = nil
-    # while input != exit
-
-    puts "1. Head Radical Pro, 2. Head Radical Touch, 3. Head Radical MPA"
-    end
+  #
+  # def call
+  #   # list_racquet_brands
+  #   list_racquet_models
   # end
+  #
+  # def list_racquet_brands
+  #   puts "List of Tennis Racquet Brands"
+  #   binding.pry
+  #   puts "1. Babolat, 2. Head, 3. Wilson"
+  # end
+  #
+  # def list_racquet_models
+  #   # input = nil
+  #   # while input != exit
+  #
+  #   puts "1. Head Radical Pro, 2. Head Radical Touch, 3. Head Radical MPA"
+  #   end
+  # # end
 
 end
