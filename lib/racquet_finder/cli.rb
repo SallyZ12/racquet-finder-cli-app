c4e012457bf75aad6ff1887c1f345189869adb95
Sally Zasloff <szasloff@gmail.com>
2017-11-26 20:16:53 +0000

    Done.

--- lib/racquet_finder/cli.rb
+++ lib/racquet_finder/cli.rb
@@ -1,28 +1,32 @@
 class RacquetFinder::CLI
 
-  #testing scraper to see if can scrape website
   def scrape_brand
     doc = Nokogiri::HTML(open("http://www.midwestsports.com"))
+    brands = doc.css("div.lefthand-nav li").first.text.split("racquets")
+    brand_array = brands.join.gsub("Racquets", "").split
     binding.pry
+    brand_array[1..9].each.with_index(1) do |brand, i|
+      puts "#{i}. #{brand}"
+    end
   end
-  #
-  # def call
-  #   # list_racquet_brands
-  #   list_racquet_models
+
+  def call
+    # list_racquet_brands
+    list_racquet_models
+  end
+
+  def list_racquet_brands
+    puts "List of Tennis Racquet Brands"
+    binding.pry
+    puts "1. Babolat, 2. Head, 3. Wilson"
+  end
+
+  def list_racquet_models
+    # input = nil
+    # while input != exit
+
+    puts "1. Head Radical Pro, 2. Head Radical Touch, 3. Head Radical MPA"
+    end
   # end
-  #
-  # def list_racquet_brands
-  #   puts "List of Tennis Racquet Brands"
-  #   binding.pry
-  #   puts "1. Babolat, 2. Head, 3. Wilson"
-  # end
-  #
-  # def list_racquet_models
-  #   # input = nil
-  #   # while input != exit
-  #
-  #   puts "1. Head Radical Pro, 2. Head Radical Touch, 3. Head Radical MPA"
-  #   end
-  # # end
 
 end
