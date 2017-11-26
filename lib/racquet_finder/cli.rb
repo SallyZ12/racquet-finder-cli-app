8e94e2bde3436d409a1e157bda83e04a167e27a2
Sally Zasloff <szasloff@gmail.com>
2017-11-26 20:25:30 +0000

    Done.

--- lib/racquet_finder/cli.rb
+++ lib/racquet_finder/cli.rb
@@ -1,14 +1,15 @@
 class RacquetFinder::CLI
 
-  def scrape_brand
-    doc = Nokogiri::HTML(open("http://www.midwestsports.com"))
-    brands = doc.css("div.lefthand-nav li").first.text.split("racquets")
-    brand_array = brands.join.gsub("Racquets", "").split
-    binding.pry
-    brand_array[1..9].each.with_index(1) do |brand, i|
-      puts "#{i}. #{brand}"
-    end
-  end
+  #tested in CLI-- resides i scraper.rb
+  # def scrape_brand
+  #   doc = Nokogiri::HTML(open("http://www.midwestsports.com"))
+  #   brands = doc.css("div.lefthand-nav li").first.text.split("racquets")
+  #   brand_array = brands.join.gsub("Racquets", "").split
+  #   brand_array[1..9].each.with_index(1) do |brand, i|
+  #     puts "#{i}. #{brand}"
+  #     # binding.pry
+  #   end
+  # end
 
   def call
     # list_racquet_brands
--- lib/racquet_finder/scraper.rb
+++ lib/racquet_finder/scraper.rb
@@ -1,10 +1,10 @@
 class RacquetFinder::Scraper
 
-  # def scrape_brand
-  #   doc = Nokogiri::HTML(open("http://www.midwestsports.com"))
-  #   brands = doc.css("div.lefthand-nav li").first.text.split("racquets")
-  #   brand_array = brands.join.gsub("Racquets", "").split
-  #   brand_array[1..9].each.with_index(1) do |brand, i|
-  #     puts "#{i}. #{brand}"
-  #   end
+  def scrape_brand
+    doc = Nokogiri::HTML(open("http://www.midwestsports.com"))
+    brands = doc.css("div.lefthand-nav li").first.text.split("racquets")
+    brand_array = brands.join.gsub("Racquets", "").split
+    brand_array[1..9].each.with_index(1) do |brand, i|
+      puts "#{i}. #{brand}"
+    end
   end
