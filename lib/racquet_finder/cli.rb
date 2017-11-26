68d4f4376f61891a25733daf5c5a8bc417340880
Sally Zasloff <szasloff@gmail.com>
2017-11-26 20:27:55 +0000

    Done.

--- lib/racquet_finder/cli.rb
+++ lib/racquet_finder/cli.rb
@@ -1,6 +1,6 @@
 class RacquetFinder::CLI
 
-  #tested in CLI-- resides i scraper.rb
+  #tested in CLI-- resides in scraper.rb
   # def scrape_brand
   #   doc = Nokogiri::HTML(open("http://www.midwestsports.com"))
   #   brands = doc.css("div.lefthand-nav li").first.text.split("racquets")
