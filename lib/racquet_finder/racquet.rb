f6457411629540b44bbfbea4390a6ac025c38064
Sally Zasloff <szasloff@gmail.com>
2017-11-29 21:25:18 +0000

    Done.

--- lib/racquet_finder/racquet.rb
+++ lib/racquet_finder/racquet.rb
@@ -11,11 +11,22 @@ class RacquetFinder::Racquet
     @price = price
   end
 
-  
+  def self.call
+    racquet=self.new
+    racquet.brand = RacquetFinder::Scraper.scrape_racquet_brands
+    racquet.model = RacquetFinder::Scraper.scrape_head_racquets
+    binding.pry
+    racquet
+  end
+
+
+
+
 
 def self.all
   @@all
 end
+
 
 
 
