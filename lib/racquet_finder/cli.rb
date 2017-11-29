e6fa59dd9fdd3b59e68ee0c97bdd42a5eb40e5e6
Sally Zasloff <szasloff@gmail.com>
2017-11-29 21:07:03 +0000

    Done.

--- lib/racquet_finder/cli.rb
+++ lib/racquet_finder/cli.rb
@@ -1,33 +1,36 @@
 class RacquetFinder::CLI
 
     def call
-      puts ""
-      puts  "Welcome to the World of Tennis Racquets"
-      puts ""
+      puts "Welcome to the World of Tennis Racquets"
     end
 
     def start
-        RacquetFinder::Scraper.new.scrape_racquet_brands
+        list_brands
         puts ""
         puts "To select a Racquet start by selecting the Brand by Number"
         puts ""
         input = gets.strip
         puts ""
+        puts "You have selected Head"
+        puts ""
 
-        RacquetFinder::Scraper.new.scrape_head_racquets
+        list_head_models
         puts ""
         puts "Select a Racquet Model"
         input = gets.strip
         puts ""
+        puts " You have selected Radical"
+        puts ""
 
-        RacquetFinder::Scraper.new.scrape_head_radical
+        list_head_radical
         puts ""
         puts "Select a Racquet"
         puts ""
         input = gets.strip
         puts ""
-        
 
+        puts "You have selected Head Graphene Radical Touch Pro at a price of $219.95"
+        puts ""
         puts "Would you like to select another racquet? Enter Y or N"
 
           input = gets.strip.downcase
@@ -37,4 +40,32 @@ class RacquetFinder::CLI
               puts "Thanks enjoy your racquet!"
             end
           end
+
+
+          def list_brands
+            puts "Racquet Brands"
+          @racquets = RacquetFinder::Scraper.new.scrape_racquet_brands
+          @racquets[1..3].each.with_index(1) do |brand, i|
+            puts "#{i}. #{brand}"
+          end
+        end
+
+
+        def list_head_models
+          puts "Head Models"
+          @head_model =   RacquetFinder::Scraper.new.scrape_head_racquets
+          @head_model[0..6].each.with_index(1) do |model, i|
+             puts "#{i}. #{model}"
+          end
+        end
+
+        def list_head_radical
+          puts "Head Radical Racquets"
+          @head_radical =     RacquetFinder::Scraper.new.scrape_head_radical
+          @head_radical[0..3].each.with_index(1) do |type, i|
+            puts "#{i}. #{type}"
+          end
+        end      
+
+
 end
--- lib/racquet_finder/scraper.rb
+++ lib/racquet_finder/scraper.rb
@@ -9,27 +9,27 @@ class RacquetFinder::Scraper
     doc = Nokogiri::HTML(open("http://www.midwestsports.com"))
     brands = doc.css("div.lefthand-nav li a").map {|brand| brand.children[0]}.join.gsub("Tennis","").split("Racquets")
 
-    brands[1..3].each.with_index(1) do |brand, i|
-      puts "#{i}. #{brand}"
-    end
+    # brands[1..3].each.with_index(1) do |brand, i|
+    #   puts "#{i}. #{brand}"
+    # end
   end
 
   def scrape_head_racquets
     doc = Nokogiri::HTML(open("http://www.midwestsports.com/head-tennis-racquets/c/104/"))
     h_models = doc.css("div.lefthand-nav li a").map {|model| model.children[0]}.join.gsub("\r","").gsub("Head","").gsub("Tennis","").gsub("Racquets","").gsub("Graphene","").split
 
-    h_models[0..6].each.with_index(1) do |model, i|
-       puts "#{i}. #{model}"
-    end
+    # h_models[0..6].each.with_index(1) do |model, i|
+    #    puts "#{i}. #{model}"
+    # end
   end
 
   def scrape_head_radical
       doc = Nokogiri::HTML(open("http://www.midwestsports.com/head-radical-tennis-racquets/c/head_radicalracquets/"))
         h_types = doc.css("div.subcatContent h3 a").map {|type| type.children[0]}.join.gsub("\r","").gsub("Head","").gsub("Graphene","").gsub("Tennis","").split("Racquet")
 
-        h_types[0..3].each.with_index(1) do |type, i|
-          puts "#{i}. #{type}"
-      end
+      #   h_types[0..3].each.with_index(1) do |type, i|
+      #     puts "#{i}. #{type}"
+      # end
     end
 
     def scrape_head_speed
