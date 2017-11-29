709d5b8bb50e36ac2bd15f105cd55f8d64f6f005
Sally Zasloff <szasloff@gmail.com>
2017-11-29 13:42:11 +0000

    Done.

--- lib/racquet_finder/cli.rb
+++ lib/racquet_finder/cli.rb
@@ -1,69 +1,16 @@
 class RacquetFinder::CLI
 
-  #for scraper.rb
-  def scrape_racquet_brands
-    doc = Nokogiri::HTML(open("http://www.midwestsports.com"))
-    brands = doc.css("div.lefthand-nav li a").map {|brand| brand.children[0]}.join.gsub("Tennis","").split("Racquets")
 
-    #for CLI
-    puts "Select Tennis Brands To Choose From:"
-    brands[1..3].each.with_index(1) do |brand, i|
-      puts "#{i}. #{brand}"
-    end
-  end
+    puts "Select A Tennis Brand:"
 
 
-  #for scraper.rb
-  def scrape_head_racquets
-    doc = Nokogiri::HTML(open("http://www.midwestsports.com/head-tennis-racquets/c/104/"))
-    h_models = doc.css("div.lefthand-nav li a").map {|model| model.children[0]}.join.gsub("\r","").gsub("Head","").gsub("Tennis","").gsub("Racquets","").gsub("Graphene","").split
-
-    #for CLI
     puts "Select a Head Tennis Racquet Model:"
 
-    h_models[0..6].each.with_index(1) do |model, i|
-       puts "#{i}. #{model}"
-    end
-  end
 
-    #For scraper.rb
-  def scrape_head_radical
-      doc = Nokogiri::HTML(open("http://www.midwestsports.com/head-radical-tennis-racquets/c/head_radicalracquets/"))
-        h_types = doc.css("div.subcatContent h3 a").map {|type| type.children[0]}.join.gsub("\r","").gsub("Head","").gsub("Graphene","").gsub("Tennis","").split("Racquet")
-
-        #for CLI
-        puts "Select a Head Racquet Type:"
-
-        h_types[0..3].each.with_index(1) do |type, i|
-          puts "#{i}. #{type}"
-      end
-    end
-
-    #for scraper.rb
-    def scrape_head_speed
-        doc = Nokogiri::HTML(open("http://www.midwestsports.com/head-speed-tennis-racquets/c/head_speedracquets/"))
-          h_types = doc.css("div.subcatContent h3 a").map {|type| type.children[0]}.join.gsub("\r","").gsub("Head","").gsub("Graphene","").gsub("Tennis","").split("Racquet")
-
-          #for CLI
-          puts "Select a Head Racquet Type:"
-
-          h_types[0..3].each.with_index(1) do |type, i|
-            puts "#{i}. #{type}"
-        end
-      end
+    puts "Select a Head Racquet Type:"
 
 
-
-    # for scraper.rb
-    def scrape_head_radical_prices
-        doc = Nokogiri::HTML(open("http://www.midwestsports.com/head-radical-tennis-racquets/c/head_radicalracquets/"))
-        h_price = doc.css("p.price strong").map {|price| price.children[0]}.join.gsub("\r","").gsub("when buying 2+","").split("$")
-
-        # for cli
-        h_price[1..4].each.with_index(1) do |type, i|
-          puts "#{i}. #{type}"
-      end
-    end
+  
 
 
     # for scraper.rb
--- lib/racquet_finder/scraper.rb
+++ lib/racquet_finder/scraper.rb
@@ -1,3 +1,58 @@
-# class RacquetFinder::Scraper
-#
-#   end
+class RacquetFinder::Scraper
+
+
+  def scrape_racquet_brands
+    doc = Nokogiri::HTML(open("http://www.midwestsports.com"))
+    brands = doc.css("div.lefthand-nav li a").map {|brand| brand.children[0]}.join.gsub("Tennis","").split("Racquets")
+
+    brands[1..3].each.with_index(1) do |brand, i|
+      puts "#{i}. #{brand}"
+    end
+  end
+
+  def scrape_head_racquets
+    doc = Nokogiri::HTML(open("http://www.midwestsports.com/head-tennis-racquets/c/104/"))
+    h_models = doc.css("div.lefthand-nav li a").map {|model| model.children[0]}.join.gsub("\r","").gsub("Head","").gsub("Tennis","").gsub("Racquets","").gsub("Graphene","").split
+
+    h_models[0..6].each.with_index(1) do |model, i|
+       puts "#{i}. #{model}"
+    end
+  end
+
+  def scrape_head_radical
+      doc = Nokogiri::HTML(open("http://www.midwestsports.com/head-radical-tennis-racquets/c/head_radicalracquets/"))
+        h_types = doc.css("div.subcatContent h3 a").map {|type| type.children[0]}.join.gsub("\r","").gsub("Head","").gsub("Graphene","").gsub("Tennis","").split("Racquet")
+
+        h_types[0..3].each.with_index(1) do |type, i|
+          puts "#{i}. #{type}"
+      end
+    end
+
+    def scrape_head_speed
+        doc = Nokogiri::HTML(open("http://www.midwestsports.com/head-speed-tennis-racquets/c/head_speedracquets/"))
+          h_types = doc.css("div.subcatContent h3 a").map {|type| type.children[0]}.join.gsub("\r","").gsub("Head","").gsub("Graphene","").gsub("Tennis","").split("Racquet")
+
+          h_types[0..3].each.with_index(1) do |type, i|
+            puts "#{i}. #{type}"
+        end
+      end
+
+
+
+      def scrape_head_radical_prices
+          doc = Nokogiri::HTML(open("http://www.midwestsports.com/head-radical-tennis-racquets/c/head_radicalracquets/"))
+          h_price = doc.css("p.price strong").map {|price| price.children[0]}.join.gsub("\r","").gsub("when buying 2+","").split("$")
+
+      
+          h_price[1..4].each.with_index(1) do |type, i|
+            puts "#{i}. #{type}"
+        end
+      end
+
+
+
+
+
+
+
+  end
