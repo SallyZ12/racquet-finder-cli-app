2a2312319bcd9256ae21f94470c2e4a2bee08e05
Sally Zasloff <szasloff@gmail.com>
2017-11-30 00:07:38 +0000

    Done.

--- lib/racquet_finder/racquet.rb
+++ lib/racquet_finder/racquet.rb
@@ -2,13 +2,67 @@ class RacquetFinder::Racquet
 
   attr_accessor :brand, :model, :type, :price
 
-  @@all =[]
+  @@all = []
+  @@BRANDS = [] #Enoch code
+  # @@BRANDS == [{
+  # name: head, url: www.url.com,
+  # models: [{name: some # model, url: www.url.com}]}]
+
+  #Enoch Code
+  @@Brands = [{name: "head", url: 'www.some.url', models: []}]
+
+  #my code followig Enoch
+  def scrape_brands
+    self.brands.each do |brand|
+      doc = Nokogiri::HTML(open(brand[:url]))
+      doc.somecss.each do |brand|
+      Racket.brands.push(some css)
+    end
+  end
+end
+
+
+#Enoch Code
+def scrape_models
+  self.brands.each do |brand|
+    doc = Nokogiri::HTML(open(brand[:url]))
+    doc.somecss.each do |model|
+      Racket.models.push(some css)
+    end
+  end
+end
+
+#Enoch Code
+def scrape_rackets
+  self.brands.each do |brand|
+    brand[:models].each do |model|
+      brand[:name] # the name of the brand
+      model[:name] # the name of the model
+      doc = Nokogiri::HTML(open(model[:url]))
+      doc.somecss.each do |racket|
+        type = scraped css of the type
+        price = scraped css of the price
+        Racket.new(brand[:name], model[:name], type, price)
+      end
+    end
+  end
+end
 
   def initialize(brand = nil, model = nil, type = nil, price = nil)
     @brand = brand
     @model = model
     @type = type
     @price = price
+  end
+
+  #Enoch Code
+  def self.brands
+    @@BRANDS
+  end
+
+  #Enoch Code
+  def self.add_brand(name, url)
+
   end
 
 
