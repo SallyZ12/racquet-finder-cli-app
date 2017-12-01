3ea585f447cb19e0f5f92861311c45f240b4f408
Sally Zasloff <szasloff@gmail.com>
2017-12-01 21:34:51 +0000

    Done.

--- lib/racquet_finder/racquet.rb
+++ lib/racquet_finder/racquet.rb
@@ -1,6 +1,6 @@
 class RacquetFinder::Racquet
 
-  attr_accessor :brand, :brand_url, :model, :model_url, :type, :price
+  attr_accessor :brand_name, :brand_url, :model_name, :model_url, :type, :price
 
   @@all = []
   @@BRANDS = [] #Enoch code
@@ -9,25 +9,38 @@ class RacquetFinder::Racquet
   # models: [{name: some # model, url: www.url.com}]}]
 
   #Enoch Code
-  @@Brands = [{brands: "head", brand_url: 'www.some.url', models: [model_name: "radical"]}]
+  # @@BRANDS = [{brand_name: "head", brand_url: "www.some.url", models: [{model_name: "radical",model_url: "www.some.url"}]}]
+
+  def initialize(brand_name = nil, model_name = nil, type_name = nil, price = nil)
+    @brand_name = brand_name
+    @model_name = model_name
+    @type_name = type_name
+    @price = price
+  end
+
+
 
   #my code followig Enoch
-  def scrape_brands
+    def scrape_brands
       doc = Nokogiri::HTML(open("http://www.midwestsports.com"))
 
 
       #this code is working for brand_names into an array
-      # brand_name = doc.css("ul.subcat li").text.split("Racquets").each do |brand|
-      #   @@BRANDS<<brand
+      brand_name = doc.css("ul.subcat li").text.split("Racquets").each do |brand|
+        binding.pry
+          # Racquet.brand.push(some css)
+          # @@BRANDS<<brand
+          # binding.pry
+        # end
 
       # brand_url = doc.css("ul.subcat li").css("a").attr("href").value.gsub("\r","").each do |url|
 
-        brand_url = doc.css("ul.subcat li a").attr("href").text.gsub("\r","")
-        binding.pry
+        # brand_url = doc.css("ul.subcat li a").attr("href").text.gsub("\r","")
+        # binding.pry
         # @@BRANDS<<url
 
       # binding.pry
-      # Racquet.brands.push(some css)
+      # Racquet.brand.push(some css)
   end
 
 
@@ -58,12 +71,7 @@ def scrape_rackets
   end
 end
 
-  def initialize(brand = nil, model = nil, type = nil, price = nil)
-    @brand = brand
-    @model = model
-    @type = type
-    @price = price
-  end
+
 
   #Enoch Code
   def self.brands
