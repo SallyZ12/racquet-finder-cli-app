de7e1e6bcd9a932fff4848436f9a7b3ccae32aec
Sally Zasloff <szasloff@gmail.com>
2017-12-08 01:26:53 +0000

    Done.

--- lib/racquet_finder.rb
+++ lib/racquet_finder.rb
@@ -13,10 +13,6 @@ require_relative './racquet_finder/racquet'
 require_relative './racquet_finder/cli'
 require_relative './racquet_finder/brand'
 
-# require_relative '../lib/racquet_finder/scraper'
-# require_relative '../lib/racquet_finder/racquet'
-# require_relative '../lib/racquet_finder/cli'
-# require_relative '../lib/racquet_finder/brand'
 
 
 module RacquetFinder
