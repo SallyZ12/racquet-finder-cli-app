76aac1baa6c4fb5740c4e0e8c52144d168aee8e7
Sally Zasloff <szasloff@gmail.com>
2017-12-08 01:26:10 +0000

    Done.

--- lib/racquet_finder.rb
+++ lib/racquet_finder.rb
@@ -4,12 +4,20 @@ require 'nokogiri'
 require 'pry'
 
 
-require "racquet_finder/version"
+# require "racquet_finder/version"
+
+require_relative './racquet_finder/version'
 
 require_relative './racquet_finder/scraper'
 require_relative './racquet_finder/racquet'
 require_relative './racquet_finder/cli'
 require_relative './racquet_finder/brand'
+
+# require_relative '../lib/racquet_finder/scraper'
+# require_relative '../lib/racquet_finder/racquet'
+# require_relative '../lib/racquet_finder/cli'
+# require_relative '../lib/racquet_finder/brand'
+
 
 module RacquetFinder
 end
